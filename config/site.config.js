452d4f2832fb189ee7aaac9c10a89582c2d25852
Yansuo <80459529+yansuogaming@users.noreply.github.com>
2025-06-24 16:11:34 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  baseDirectory: ['/Games', '/PhanMem'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:yansuogaming@gmail.com',
